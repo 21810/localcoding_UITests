1206f7e078c9079a48dfaae09985f12963e1d9e3
“Yulia <yulia.harbacheuskaya@gmail.com>
2021-04-19 21:51:43 -0400

    fix: added eslint-wdio

--- test/pages/users/profile.page.js
+++ test/pages/users/profile.page.js
@@ -1,12 +1,14 @@
 import BasePage from '../base.page';
 
 class ProfilePortalPage extends BasePage {
-    get iconUser () { return $('.ant-avatar-square');}
+  get iconUser() {
+    return $('.ant-avatar-square');
+  }
 
-    isOpen () {
-        expect(browser).toHaveUrlContaining('profile');
-        expect(this.iconUser).toBeDisplayed();
-    }
+  isOpen() {
+    expect(browser).toHaveUrlContaining('profile');
+    expect(this.iconUser).toBeDisplayed();
+  }
 }
 
 export default new ProfilePortalPage();

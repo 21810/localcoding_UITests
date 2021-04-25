73e58cfaa4485dbe66439705c46ce70db23cebd0
“Yulia <yulia.harbacheuskaya@gmail.com>
2021-04-25 18:58:47 -0400

    feat: setup allure

--- config/hooks.conf.js
+++ config/hooks.conf.js
@@ -2,13 +2,19 @@ import LoginPage from '../test/pageobjects/login.page';
 import ProfilePage from '../test/pageobjects/portal/profile.portal.page';
 
 module.exports = {
-    before: function (capabilities, specs) {
-        browser.addCommand('login', function (username, password) {
-            LoginPage.open();
-            LoginPage.setEmail(username);
-            LoginPage.setPassword(password);
-            LoginPage.clickSubmitButton();
-            ProfilePage.isOpen();
-        })
+  before: function (capabilities, specs) {
+    browser.addCommand('login', function (username, password) {
+      LoginPage.open();
+      LoginPage.setEmail(username);
+      LoginPage.setPassword(password);
+      LoginPage.clickSubmitButton();
+      ProfilePage.isOpen();
+    });
+  },
+
+  afterTest: function(test, context, { error, result, duration, passed, retries }) {
+    if (error) {
+      browser.takeScreenshot();
     }
-}
+  }
+};

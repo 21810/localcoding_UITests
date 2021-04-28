8725f113c4c61e11f247eff587a554fd143e7c1f
Elena-Sergienko <elena.serg.us@gmail.com>
2021-04-27 17:18:16 -0700

    upd scripts, specs

--- config/base.conf.js
+++ config/base.conf.js
@@ -2,14 +2,15 @@ module.exports = {
   specs: [
     './test/specs/**/*.js',
   ],
-  // exclude: [
-  //     './test/specs/navigation.spec.js'
-  // ],
+  exclude: [
+      './test/specs/navigation.spec.js'
+  ],
   suites: {
     smoke: ['./test/specs/smoke/**/*.js'],
     challenges: ['./test/specs/regression/challenges/**/*spec.js'],
     navigation: ['./test/specs/regression/navigation/**/*spec.js'],
-    users: ['./test/specs/regression/users/**/*spec.js']
+    users: ['./test/specs/regression/users/**/*spec.js'],
+    registration: ['./test/specs/regression/users/registration/*spec.js']
   },
 
   logLevel: 'info',

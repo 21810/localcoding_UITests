49cb573bbe66fc347cf7b38410b09466d2d15026
meyta2020 <tmeysner@gmail.com>
2021-05-14 16:39:16 -0500

    shopProductTests

--- test/pages/shop/shopProduct.page.js
+++ test/pages/shop/shopProduct.page.js
@@ -5,17 +5,36 @@ class ShopProductPage extends BasePage {
   get userNameDropDown() {return $('//span[@class=\'anticon anticon-down\']/..');}
   get shopInDropDown() {return $('//li//*[contains(text(),"Shop")]');}
   get headerProducts() {return $('//h1[contains(text(),"Products")]');}
+  get javaScriptSyntaxCoursePage () {return $('//div//h1[contains(text(),"JavaScript Syntax")]');}
+
+  get startLearningBtn () {return $('//a[contains(text(),"Start learning")]');}
+
   get javaScriptSyntaxSelfLearn () {return $('//h3//a[contains(text(),"JavaScript Syntax самостоятельно")]');}
   get javaScriptSyntaxWithTeacher () {return $('//h3//a[contains(text(),"JavaScript Syntax с преподавателем")]');}
   get javaScriptPractice () {return $('//h3//a[contains(text(),"JavaScript Practice")]');}
   get reactJS () {return $('//h3//a[contains(text(),"React JS")]');}
   get fullstackReactProject () {return $('//h3//a[contains(text(),"Fullstack React Project")]');}
-  get startLearningBtn () {return $('//a[contains(text(),"Start learning")]');}
-  get javaScriptSyntaxCoursePage () {return $('//div//h1[contains(text(),"JavaScript Syntax")]');}
+
+  get javaScriptSyntaxSelfLearnDescription () {return $('//div[contains(text(),"Подготовит вас к нашим профессиональным курсам.")]');}
+  get javaScriptSyntaxWithTeacherDescription () {return $('//*[contains(text(),"1 месяц по 3 занятия в неделю.")]');}
+  get javaScriptPracticeDescription () {return $('//p[contains(text(),"2 месяца по 2 занятия в неделю.")]');}
+  get reactJSDescription () {return $('//*[contains(text(),"Длительность 2 месяца.")]');}
+  get fullstackReactProjectDescription () {return $('//*[contains(text(),"Доступно только выпускникам курса React JS")]');}
 
   open() {
     super.open(url.shop);
   }
+
+  goToShopPage(){
+    this.userNameDropDown.click();
+    this.shopInDropDown.waitForClickable();
+    this.shopInDropDown.click();
+  }
+
+
+
+
+
 }
 
 export default new ShopProductPage();
--- test/specs/regression/shop/shopProduct.spec.js
+++ test/specs/regression/shop/shopProduct.spec.js
@@ -2,32 +2,25 @@ import ShopProductPage from '../../../pages/shop/shopProduct.page';
 import userData, {users} from '../../../data/users.data';
 import HomePage from '../../../pages/home.page';
 import LoginPage from '../../../pages/users/login.page';
-
+import expected from '../../../data/expected.json';
 
 describe('SHOP PRODUCT PAGE - POSITIVE', () => {
 
-  describe('SHOP PRODUCT PAGE - ROLE NEW BASE FUNCTIONAL', () => {
+  describe('SHOP PRODUCT PAGE - ROLE NEW', () => {
     before(() => {
       HomePage.open();
       HomePage.clickLoginLink();
       LoginPage.userLogin(users.new.email, users.new.password);
     });
 
-    it('TC SP-002 Verify that Shop page is available for user with new role', () => {
-      ShopProductPage.userNameDropDown.click();
-      ShopProductPage.shopInDropDown.waitForClickable();
-      ShopProductPage.shopInDropDown.click();
-      expect(ShopProductPage.headerProducts).toBeDisplayed();
-    });
-  });
-
-  describe('SHOP PRODUCT PAGE - ROLE NEW', () => {
-    before(() => {
-      ShopProductPage.open();
-    });
-
     after(() => {
       browser.execute('window.localStorage.clear()');
+    });
+
+    it('TC SP-002 Verify that Shop page is available for user with new role', () => {
+      ShopProductPage.goToShopPage();
+      expect(ShopProductPage.headerProducts).toBeDisplayed();
+      expect(browser.getUrl()).toEqual(expected.url.shopUrl);
     });
 
     it('TC SP-003-1 Verify that Java Script Syntax SelfLearn is available for user with new role', () => {
@@ -46,33 +39,54 @@ describe('SHOP PRODUCT PAGE - POSITIVE', () => {
       expect(ShopProductPage.reactJS).toBeDisplayed();
     });
 
+    it('TC SP-020 Verify Description for ReactJS course', () => {
+      expect(ShopProductPage.reactJSDescription).toHaveText(expected.courseDescription.reactJS);
+    });
+
     it('TC SP-003-5 Verify that Fullstack React Project is available for user with new role', () => {
       expect(ShopProductPage.fullstackReactProject).toBeDisplayed();
     });
+
+    it('TC SP-017 Verify Description for JavaScript Syntax самостоятельно course', () => {
+      expect(ShopProductPage.javaScriptSyntaxSelfLearnDescription).toHaveText(expected.courseDescription.javaScriptSyntaxSelfLearn);
+    });
+
+    it('TC SP-018 Verify Description for Java Script Syntax With Teacher course', () => {
+      expect(ShopProductPage.javaScriptSyntaxWithTeacherDescription).toHaveText(expected.courseDescription.javaScriptSyntaxWithTeacher);
+    });
+
+    it('TC SP-019 Verify Description for JavaScript Practice course', () => {
+      ShopProductPage.javaScriptPractice.waitForClickable();
+      ShopProductPage.javaScriptPractice.click();
+      expect(ShopProductPage.javaScriptPracticeDescription).toHaveText(expected.courseDescription.javaScriptPractice);
+      browser.back();
+    });
+
+    it('TC SP-020 Verify Description for Fullstack React JS course', () => {
+      expect(ShopProductPage.reactJSDescription).toHaveText(expected.courseDescription.reactJS);
+    });
+
+    it('TC SP-021 Verify Description for Fullstack React Project course', () => {
+      expect(ShopProductPage.fullstackReactProjectDescription).toHaveText(expected.courseDescription.fullstackReactProject);
+    });
+
   });
 
-  describe('SHOP PRODUCT PAGE - ROLE LEARNER BASE FUNCTIONAL', () => {
+  describe('SHOP PRODUCT PAGE - ROLE LEARNER', () => {
     before(() => {
       HomePage.open();
       HomePage.clickLoginLink();
       LoginPage.userLogin(users.leaner.email, users.leaner.password);
     });
 
-    it('TC SP-005 Verify that Shop page is available for user with role Learner', () => {
-      ShopProductPage.userNameDropDown.click();
-      ShopProductPage.shopInDropDown.waitForClickable();
-      ShopProductPage.shopInDropDown.click();
-      expect(ShopProductPage.headerProducts).toBeDisplayed();
-    });
-  });
-
-  describe('SHOP PRODUCT PAGE - ROLE LEARNER', () => {
-    before(() => {
-      ShopProductPage.open();
-    });
-
     after(() => {
       browser.execute('window.localStorage.clear()');
+    });
+
+    it('TC SP-005 Verify that Shop page is available for user with role Learner', () => {
+      ShopProductPage.goToShopPage();
+      expect(ShopProductPage.headerProducts).toBeDisplayed();
+      expect(browser.getUrl()).toEqual(expected.url.shopUrl);
     });
 
     it('TC SP-006-1 Verify that Java Script Syntax SelfLearn is available for user with role Learner', () => {
@@ -101,7 +115,134 @@ describe('SHOP PRODUCT PAGE - POSITIVE', () => {
     });
   });
 
+  describe('SHOP PRODUCT PAGE - ROLE STUDENT', () => {
+    before(() => {
+      HomePage.open();
+      HomePage.clickLoginLink();
+      LoginPage.userLogin(users.student.email, users.student.password);
+    });
 
+    after(() => {
+      browser.execute('window.localStorage.clear()');
+    });
+
+    it('TC SP-008 Verify that Shop page is available for user with role Student', () => {
+      ShopProductPage.goToShopPage();
+      expect(ShopProductPage.headerProducts).toBeDisplayed();
+      expect(browser.getUrl()).toEqual(expected.url.shopUrl);
+    });
+
+    it('TC SP-009-1 Verify that Java Script Syntax SelfLearn is available for user with role Student', () => {
+      expect(ShopProductPage.javaScriptSyntaxSelfLearn).toBeDisplayed();
+    });
+
+    it('TC SP-009-2 Verify that Java Script Syntax With Teacher is available for user with role Student', () => {
+      expect(ShopProductPage.javaScriptSyntaxWithTeacher).toBeDisplayed();
+    });
+
+    it('TC SP-009-3 Verify that JavaScript Practice is available for user with role Student', () => {
+      expect(ShopProductPage.javaScriptPractice).toBeDisplayed();
+    });
+
+    it('TC SP-009-4 Verify that ReactJS is available for user with role Student', () => {
+      expect(ShopProductPage.reactJS).toBeDisplayed();
+    });
+
+    it('TC SP-009-5 Verify that Fullstack React Project is available for user with role Student', () => {
+      expect(ShopProductPage.fullstackReactProject).toBeDisplayed();
+    });
+
+    it('TC SP-010 Verify that JavaScript Syntax самостоятельно for user with role Student is available and free', () => {
+      ShopProductPage.startLearningBtn.click();
+      expect(ShopProductPage.javaScriptSyntaxCoursePage).toBeDisplayed();
+    });
+  });
+
+  describe('SHOP PRODUCT PAGE - ROLE TEACHER', () => {
+    before(() => {
+      HomePage.open();
+      HomePage.clickLoginLink();
+      LoginPage.userLogin(users.teacher.email, users.teacher.password);
+    });
+
+    after(() => {
+      browser.execute('window.localStorage.clear()');
+    });
+
+    it('TC SP-011 Verify that Shop page is available for user with role Teacher', () => {
+      ShopProductPage.goToShopPage();
+      expect(ShopProductPage.headerProducts).toBeDisplayed();
+      expect(browser.getUrl()).toEqual(expected.url.shopUrl);
+    });
+
+    it('TC SP-012-1 Verify that Java Script Syntax SelfLearn is available for user with role Teacher', () => {
+      expect(ShopProductPage.javaScriptSyntaxSelfLearn).toBeDisplayed();
+    });
+
+    it('TC SP-012-2 Verify that Java Script Syntax With Teacher is available for user with role Teacher', () => {
+      expect(ShopProductPage.javaScriptSyntaxWithTeacher).toBeDisplayed();
+    });
+
+    it('TC SP-012-3 Verify that JavaScript Practice is available for user with role Teacher', () => {
+      expect(ShopProductPage.javaScriptPractice).toBeDisplayed();
+    });
+
+    it('TC SP-012-4 Verify that ReactJS is available for user with role Teacher', () => {
+      expect(ShopProductPage.reactJS).toBeDisplayed();
+    });
+
+    it('TC SP-012-5 Verify that Fullstack React Project is available for user with role Teacher', () => {
+      expect(ShopProductPage.fullstackReactProject).toBeDisplayed();
+    });
+
+    it('TC SP-013 Verify that JavaScript Syntax самостоятельно for user with role Teacher is available and free', () => {
+      ShopProductPage.startLearningBtn.click();
+      expect(ShopProductPage.javaScriptSyntaxCoursePage).toBeDisplayed();
+    });
+  });
+
+  describe('SHOP PRODUCT PAGE - ROLE ADMIN', () => {
+    before(() => {
+      HomePage.open();
+      HomePage.clickLoginLink();
+      LoginPage.userLogin(users.admin.email, users.admin.password);
+    });
+
+    after(() => {
+      browser.execute('window.localStorage.clear()');
+    });
+
+    it('TC SP-014 Verify that Shop page is available for user with role Admin', () => {
+      ShopProductPage.goToShopPage();
+      expect(ShopProductPage.headerProducts).toBeDisplayed();
+      expect(browser.getUrl()).toEqual(expected.url.shopUrl);
+    });
+
+    it('TC SP-015-1 Verify that Java Script Syntax SelfLearn is available for user with role Admin', () => {
+      expect(ShopProductPage.javaScriptSyntaxSelfLearn).toBeDisplayed();
+    });
+
+    it('TC SP-015-2 Verify that Java Script Syntax With Teacher is available for user with role Admin', () => {
+      expect(ShopProductPage.javaScriptSyntaxWithTeacher).toBeDisplayed();
+    });
+
+    it('TC SP-015-3 Verify that JavaScript Practice is available for user with role Admin', () => {
+      expect(ShopProductPage.javaScriptPractice).toBeDisplayed();
+    });
+
+    it('TC SP-015-4 Verify that ReactJS is available for user with role Admin', () => {
+      expect(ShopProductPage.reactJS).toBeDisplayed();
+    });
+
+    it('TC SP-015-5 Verify that Fullstack React Project is available for user with role Admin', () => {
+      expect(ShopProductPage.fullstackReactProject).toBeDisplayed();
+    });
+
+    it('TC SP-016 Verify that JavaScript Syntax самостоятельно for user with role Admin is available and free', () => {
+      ShopProductPage.startLearningBtn.click();
+      expect(ShopProductPage.javaScriptSyntaxCoursePage).toBeDisplayed();
+    });
+  });
 
 });
 
